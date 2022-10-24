ff32f768089d28651b6284563ea74802068315f1
frknciloglu <clglufurkan@gmail.com>
2022-10-24 14:30:44 +0300

    lesson3:Model_binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Furkan';
+
+  items = [
+    { description: 'kahvaltı', action: 'no' },
+    { description: 'Sinema', action: 'yes' },
+    { description: 'çay', action: 'yes' },
+    { description: 'kahve', action: 'no' },
+  ];
 }
